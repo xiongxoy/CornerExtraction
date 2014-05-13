a68624f8bac4eb7632b018a9adf83c9408104261
xiongxoy <xiong.xoy@gmail.com>
2014-05-13 08:40:41 +0800

    Add Unit Test

---
+++ TestCornerExtraction.py
@@ -0,0 +1,23 @@
+'''
+Created on 2014-4-29
+
+@author: Zhou Xiong
+'''
+import unittest
+import research.corner.CornerExtration.get_sub_image
+
+class Test(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def testCornerExtraction(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testCornerExtraction']
+    unittest.main()
