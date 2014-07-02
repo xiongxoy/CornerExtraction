f401225765887764e59fd4fd42ae75028b6d57ef
xiongxoy <xiong.xoy@gmail.com>
2014-07-02 17:28:04 +0800

    Move main to top level

---
+++ main.py
@@ -0,0 +1,59 @@
+'''
+Created on 2014-7-2
+
+@author: zx
+'''
+import cv2
+import logging
+import copy
+
+from research.corner.corner_extraction import get_sub_image, CornerExtractor
+from research.corner.global_variable import GlobalVariable
+
+
+def perspectvie_trans_demo():
+    from research.corner.image_transformer import ImageTransformer
+    from research.corner.rectangle_drawer import RectangleDrawer
+
+    # Create a black image, a window and bind *draw_rectangle* to it
+    cv2.namedWindow('image')
+    cv2.setMouseCallback('image', RectangleDrawer.draw_rectangle)
+
+    # Read image
+    GlobalVariable.original_image = cv2.imread('img/corridor.jpg')
+    if GlobalVariable.original_image is None:
+        logging.error('Image Not Found')
+        assert 0
+    GlobalVariable.original_image = cv2.resize(GlobalVariable.original_image,
+                                               (0, 0), fx=0.3, fy=0.3)
+    # image for display
+    GlobalVariable.display_image = copy.deepcopy(GlobalVariable.original_image)
+    # Show image
+    cv2.imshow("image", GlobalVariable.display_image)
+    # Wait for user input,
+    # the target rectangle should be drawn before hitting carret
+    key = cv2.waitKey(0)
+    # if carret is hit
+    if key == ord('\r'):
+        # extract target region
+        sub_image = get_sub_image(GlobalVariable.original_image,
+                                  GlobalVariable.start_point,
+                                  GlobalVariable.end_point)
+        # transform image
+        transformer = ImageTransformer(sub_image)
+        transformed_image = transformer.transform()
+        # show result and wait
+        cv2.imshow("transformed", transformed_image)
+        cv2.waitKey(0)
+
+    # clean up windows
+    cv2.destroyAllWindows()
+
+
+
+
+if __name__ == '__main__':
+    print 'Hello'
+    logging.error("Good Start")
+    perspectvie_trans_demo()
+    ransac_corner_extraction_demo()
