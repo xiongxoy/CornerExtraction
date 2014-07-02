457bccbb4c272a37166aee00a4ee9b3cc06c12a1
xiongxoy <xiong.xoy@gmail.com>
2014-07-02 17:17:02 +0800

    Add Test for Ransac

---
+++ test/TestRANSAC.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Created on 2014-6-28
+
+@author: Zhou Xiong
+'''
+import unittest
+
+
+class Test(unittest.TestCase):
+
+    def test_delete_one_edge(self):
+        '''
+            此函数测试RANSAC的核心部分，需要从contour中删除和指定点
+            在同一直线上的点
+        '''
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
