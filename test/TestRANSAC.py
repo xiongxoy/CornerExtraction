4e87a2a6372ef0eb406abadb87d087638bdf5d99
xiongxoy <xiong.xoy@gmail.com>
2014-07-03 19:04:15 +0800

    Add test for delete_one_edge, core of RANSAC

--- test/TestRANSAC.py
+++ test/TestRANSAC.py
@@ -23,11 +23,9 @@ class Test(unittest.TestCase):
     def test_ransac_on_rectangle(self):
         print GlobalVariable.original_image
         rect = line(10, 10, 10, 100)
-        rect.extend(line(10, 100, 100, 100))
-        rect.extend(line(100, 100, 10, 100))
-        rect.extend(line(10, 100, 10, 10))
+        rect.extend(line(100, 10, 10, 10))
         rect = np.asarray(rect, np.int)
-        print self.analyzer.extract_lines(rect)
+        print self.analyzer.extract_lines(rect, 2)
 
     def test_distance_from_line(self):
         '''
@@ -41,11 +39,17 @@ class Test(unittest.TestCase):
 
     def test_delete_one_edge(self):
         '''
-            此函数测试RANSAC的核心部分，需要从contour中删除和指定点
-            在同一直线上的点
+        此函数测试RANSAC的核心部分，需要从contour中删除和指定点
+        在同一直线上的点
         '''
-        points = line(0, 0, 0, 10)
-        points.extend(line(10, 0, 10, 10))
+        pass
+
+    def test_set_adjacent_inliner(self):
+        '''
+        测试指定点周围的点是否被恰当的设定了idx
+        '''
+        points = line(0, 0, 0, 100)
+        points.extend(line(10, 0, 10, 100))
         inliner_idx = [-1] * len(points)
         n = np.asarray([0, 10], np.float)
         a = np.asarray([0, 0], np.float)
@@ -54,7 +58,7 @@ class Test(unittest.TestCase):
         print len(points), points
         print len(inliner_idx), inliner_idx
         inliner_idx = filter(lambda x: x != -1, inliner_idx)
-        self.assertEqual(len(inliner_idx), 11)
+        self.assertEqual(len(inliner_idx), len(points) / 2)
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
