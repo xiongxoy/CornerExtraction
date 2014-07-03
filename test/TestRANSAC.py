ee208ce1992efb414d94137ca0ea01dcaeb2545e
xiongxoy <xiong.xoy@gmail.com>
2014-07-03 17:24:07 +0800

    Add Tests for RANSAC

--- test/TestRANSAC.py
+++ test/TestRANSAC.py
@@ -6,15 +6,55 @@ Created on 2014-6-28
 @author: Zhou Xiong
 '''
 import unittest
+import numpy as np
+
+from research.util import line, distance_from_point_to_line
+from research.corner.corner_extraction import ContourAnalyzerRANSAC
+from research.corner.global_variable import GlobalVariable
 
 
 class Test(unittest.TestCase):
+
+    def setUp(self):
+        GlobalVariable.original_image = np.zeros((200, 200), np.float)
+        self.analyzer = ContourAnalyzerRANSAC()
+
+    # FIXME: 直线没有被正确的提取
+    def test_ransac_on_rectangle(self):
+        print GlobalVariable.original_image
+        rect = line(10, 10, 10, 100)
+        rect.extend(line(10, 100, 100, 100))
+        rect.extend(line(100, 100, 10, 100))
+        rect.extend(line(10, 100, 10, 10))
+        rect = np.asarray(rect, np.int)
+        print self.analyzer.extract_lines(rect)
+
+    def test_distance_from_line(self):
+        '''
+        测试点到直线距离计算的准确性
+        '''
+        n = np.asarray([0, 10], np.float)
+        a = np.asarray([0, 0], np.float)
+        point = np.asarray([1.5, 10], np.float)
+        d = distance_from_point_to_line(n, a, point)
+        np.testing.assert_almost_equal(d, 1.5)
 
     def test_delete_one_edge(self):
         '''
             此函数测试RANSAC的核心部分，需要从contour中删除和指定点
             在同一直线上的点
         '''
+        points = line(0, 0, 0, 10)
+        points.extend(line(10, 0, 10, 10))
+        inliner_idx = [-1] * len(points)
+        n = np.asarray([0, 10], np.float)
+        a = np.asarray([0, 0], np.float)
+        inliner_idx = self.analyzer.set_adjacent_inliner(points,
+                                                         inliner_idx, 3, n, a)
+        print len(points), points
+        print len(inliner_idx), inliner_idx
+        inliner_idx = filter(lambda x: x != -1, inliner_idx)
+        self.assertEqual(len(inliner_idx), 11)
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
